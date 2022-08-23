622f4b747fe577155a761eb3551959d26c2b5527
mohamedabdelmaksod87 <mohamedabdelmaksod1987@gmail.com>
2022-08-23 08:14:23 +0200

    deploy frontend

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
+aws s3 cp --recursive --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./build s3://my-react-frontend-app/
